e538a65a2757f1ce41ca289d3f7eadf0654cd0b6
Cwdrian <xcharlesdrian@gmail.com>
2024-03-20 19:39:23 -0400

    front end skelly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
+import React from 'react';
+import { Link, Route, Routes } from "react-router-dom";
+import './index.css';
+import Home from "./pages/home";
+import Countries from "./pages/countries";
+import Wishlist from "./pages/wishlist";
+import Destination from "./pages/destinations";
+
 function App() {
   return (
     <div className="App">
+      <header>
+        <h1 className="main-title">The Voyagers Vault</h1>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/countries">Countries</Link></li>
+            <li><Link to="/wishlist">Wishlist</Link></li>
+            <li><Link to="/destination">Destination</Link></li>
+          </ul>
+        </nav>
+      </header>
       <Routes>
-        
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/countries"} element={<Countries />} />
+        <Route path={"/destination"} element={<Destination />} />
+        <Route path={"/wishlist"} element={<Wishlist />} />
       </Routes>
     </div>
   );
