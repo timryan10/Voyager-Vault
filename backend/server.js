a3c0dd01563b49264feac0a9bcd7793c9ac1e970
Cwdrian <xcharlesdrian@gmail.com>
2024-03-20 16:12:13 -0400

    connected DB

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,19 @@
-import "dotenv/config";
-import express from "express"
-import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
