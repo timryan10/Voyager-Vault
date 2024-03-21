5515233b87ba57d4a4aadfbff3af8b5dd3c6c701
Melanie Allmond <melanie.allmond@gmail.com>
2024-03-21 17:23:56 -0400

    Added notes for backend changes needed

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt')
 
 const { User } = db
 
+
+// need post: Login & post: Register
 router.post('/', async (req, res) => {
     let { password, ...rest } = req.body;
     const user = await User.create({
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
 import dotenv from 'dotenv';
+// import userController from './controllers/user';
+
 
 dotenv.config()
 
@@ -11,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
+// app.use('/user', userController)
 
 // start the Express server
 app.listen(PORT, () => {
