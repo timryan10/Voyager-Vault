1ce292559e9022ec44ada279bdfbfe9b22b512ef
Tim <rtim10@ymail.com>
2024-03-20 22:09:13 -0400

    pushing to main

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import records from "./routes/record.js";
 import dotenv from 'dotenv';
 
+dotenv.config()
+
 const PORT = process.env.PORT || 5050;
 const app = express();
 
