e4ee13930c7f4ce765d94a0fc81aa1e067c75544
Melanie Allmond <melanie.allmond@gmail.com>
2024-03-23 12:00:22 -0400

    Changes to homepage and css

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -13,30 +13,30 @@ return (
             </div>
 
             <div>
-                <img className="banner-img" src="https://media.istockphoto.com/id/1366403216/photo/german-shepherd.webp?b=1&s=170667a&w=0&k=20&c=Fmstk2OPzftZtkRiU9rA61mRaWzpK_1Y7Q-FOVqvVU4=" />
+                <img className="banner-img" src="https://www.odysseys-unlimited.com/wp-content/uploads/2023/05/Lead-AdobeStock_290053056-scaled.jpeg" alt="Ireland Odysseys Unlimited by Irina Schmidt on adobe stock photos" />
                 <div className="image-tiles d-flex justify-content-center align-items-center">
                     <div className="circle-image position-relative">
-                        <a href="/pages/countries"><img className="rounded-circle" src="https://media.istockphoto.com/id/493218065/photo/european-continent-marked-with-flags.jpg?s=612x612&w=0&k=20&c=Tis1r0KBQUs18vg7f4FEHqlG9Zgik6ClW1F_AFGfUDw=" alt="European continent with flags from istockphoto" /></a>
+                        <a href="/pages/countries"><img className="rounded-circle" src="https://st2.depositphotos.com/3591429/5996/i/450/depositphotos_59969927-stock-photo-different-countries-united-with-flags.jpg" alt="Country flags from depositphoto.com" /></a>
                         <div className="middle">
-                            <Link to="/countries"><div className="text">Countries</div></Link>
+                            {/* <Link to="/countries"><div className="text">Countries</div></Link> */}
                         </div>
                     </div>
                     <div className="circle-image position-relative">
-                        <a href="/pages/destinations"><img className="rounded-circle" src="https://media.istockphoto.com/id/1424070375/photo/savannah-cat-sits-on-a-pedestal-pillow-against-a-background-of-greenery.jpg?s=612x612&w=0&k=20&c=48u686oEWUSpkbfVeqQOEcimNl1u0LMEsUSrA9y_ncY=" /></a>
+                        <a href="/pages/destinations"><img className="rounded-circle" src="https://t4.ftcdn.net/jpg/03/08/38/83/360_F_308388341_kzxK7d31ZYO7FmBClxJmYuZZb6dn6a3g.jpg" alt="Tourist images from adobe stock photos" /></a>
                         <div className="middle">
-                            <Link to="/destinations"><div className="text">Destinations</div></Link>
+                            {/* <Link to="/destinations"><div className="text">Destinations</div></Link> */}
                         </div>
                     </div>
                     <div className="circle-image position-relative">
-                        <img className="rounded-circle" src="https://www.worldanimalprotection.us/sites/default/files/styles/600x400/public/media/GettyImages-171152147_0.jpg?h=2a7d4302&itok=Qlo1dXxe" />
+                        <a href="/pages/wishlist"><img className="rounded-circle" src="https://media.istockphoto.com/id/1058606496/photo/flat-lay-of-toy-airplane-with-pen-and-white-clean-paper-notepad-on-vivid-yellow-background.jpg?s=612x612&w=0&k=20&c=D2do0jwVaWGbr903qKeTegWr1Pvb7mCCE-Hj1ZuMVGU=" alt="Travel wish list from istockphoto.com" /></a>
                         <div className="middle">
-                            <Link to="/wishlist"><div className="text">Wishlist</div></Link>
+                            {/* <Link to="/wishlist"><div className="text">Wishlist</div></Link> */}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-  );
+    );
 }
 
 export default Home
