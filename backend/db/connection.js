39b9e8f12e9d62d9b260eb0bf298b4543c45aced
Cwdrian <xcharlesdrian@gmail.com>
2024-03-25 18:22:00 -0400

    jwt and bcrypt

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,28 +1,25 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+const db = require("../models");
+const jwt = require('jsonwebtoken');
 
+const { User } = db;
 
-const uri = process.env.ATLAS_URI || "";
-const client = new MongoClient("mongodb+srv://timryan025:admin@voyager-vault.pqbssm1.mongodb.net/?retryWrites=true&w=majority&appName=Voyager-Vault", {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-
-try {
-  // Connect the client to the server
-  await client.connect();
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log(
-  "Pinged your deployment. You successfully connected to MongoDB!"
-  );
-} catch(err) {
-  console.error(err);
+async function defineCurrentUser(req, res, next) {
+    try {
+        const authHeader = req.headers.authorization;
+        if (authHeader && authHeader.startsWith('Bearer ')) {
+            const token = authHeader.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const { id } = decoded;
+            const user = await User.findById(id);
+            req.currentUser = user;
+        } else {
+            req.currentUser = null;
+        }
+        next();
+    } catch (err) {
+        req.currentUser = null;
+        next(err);
+    }
 }
 
-let db = client.db("employees");
-
-export default db;
+module.exports = defineCurrentUser;
