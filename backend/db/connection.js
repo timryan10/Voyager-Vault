e5ce85a9801b8167955edac286a1ca9707d64910
Tim <rtim10@ymail.com>
2024-03-25 18:13:15 -0400

    signin

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,13 +1,8 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-<<<<<<< HEAD
-/*const uri = process.env.ATLAS_URI || ""; */
-const client = new MongoClient("mongodb+srv://timryan025:admin@voyager-vault.pqbssm1.mongodb.net/", {
-=======
 
 const uri = process.env.ATLAS_URI || "";
 const client = new MongoClient("mongodb+srv://timryan025:admin@voyager-vault.pqbssm1.mongodb.net/?retryWrites=true&w=majority&appName=Voyager-Vault", {
->>>>>>> 04269a3799ce52ebddc79cf2de0de9b7797cfa78
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
