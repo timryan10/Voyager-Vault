c1259b062210ae44e8fec8f6014b8db8b4458a56
tsmit275 <taylor.lsmith03@gmail.com>
2024-03-24 21:30:17 -0400

    added some styling to homepage

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -5,9 +5,9 @@ const Home = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slides = [
     "https://www.odysseys-unlimited.com/wp-content/uploads/2023/05/Lead-AdobeStock_290053056-scaled.jpeg",
-    "https://images.pexels.com/photos/1010657/pexels-photo-1010657.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/3727255/pexels-photo-3727255.jpeg",
     "https://images.pexels.com/photos/3601453/pexels-photo-3601453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    "https://images.pexels.com/photos/869258/pexels-photo-869258.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    "https://images.pexels.com/photos/869258/pexels-photo-869258.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   ];
 
   useEffect(() => {
@@ -21,7 +21,7 @@ const Home = () => {
   return (
     <div>
       <div>
-        <h1>Voyager Vault</h1>
+        <h1 className="website-title">Voyager Vault</h1>
         <NavBar />
       </div>
       <div className="carousel slide" data-ride="carousel">
@@ -29,29 +29,57 @@ const Home = () => {
           {slides.map((slide, index) => (
             <div
               key={index}
-              className={`carousel-item ${index === currentSlide ? "active" : ""}`}
+              className={`carousel-item ${
+                index === currentSlide ? "active" : ""
+              }`}
             >
-              <img className="d-block w-100" src={slide} alt={`Slide ${index + 1}`} />
+              <img
+                className="d-block w-100"
+                src={slide}
+                alt={`Slide ${index + 1}`}
+              />
             </div>
           ))}
         </div>
       </div>
-
+      <div>
+        <p className="our-mission">
+          Unlocking Travel Stories, Inspiring Destinations
+        </p>
+      </div>
       <div className="image-tiles d-flex justify-content-center align-items-center">
         <div className="circle-image position-relative">
-          <a href="/pages/countries"><img className="rounded-circle" src="https://st2.depositphotos.com/3591429/5996/i/450/depositphotos_59969927-stock-photo-different-countries-united-with-flags.jpg" alt="Country flags from depositphoto.com" /></a>
+          <a href="/pages/countries">
+            <img
+              className="rounded-circle"
+              src="https://st2.depositphotos.com/3591429/5996/i/450/depositphotos_59969927-stock-photo-different-countries-united-with-flags.jpg"
+              alt="Country flags from depositphoto.com"
+            />
+          </a>
           <div className="middle">
             {/* <Link to="/countries"><div className="text">Countries</div></Link> */}
           </div>
         </div>
         <div className="circle-image position-relative">
-          <a href="/pages/destinations"><img className="rounded-circle" src="https://t4.ftcdn.net/jpg/03/08/38/83/360_F_308388341_kzxK7d31ZYO7FmBClxJmYuZZb6dn6a3g.jpg" alt="Tourist images from adobe stock photos" /></a>
+          <a href="/pages/destinations">
+            <img
+              className="rounded-circle"
+              src="https://t4.ftcdn.net/jpg/03/08/38/83/360_F_308388341_kzxK7d31ZYO7FmBClxJmYuZZb6dn6a3g.jpg"
+              alt="Tourist images from adobe stock photos"
+            />
+          </a>
           <div className="middle">
             {/* <Link to="/destinations"><div className="text">Destinations</div></Link> */}
           </div>
         </div>
         <div className="circle-image position-relative">
-          <a href="/pages/wishlist"><img className="rounded-circle" src="https://media.istockphoto.com/id/1058606496/photo/flat-lay-of-toy-airplane-with-pen-and-white-clean-paper-notepad-on-vivid-yellow-background.jpg?s=612x612&w=0&k=20&c=D2do0jwVaWGbr903qKeTegWr1Pvb7mCCE-Hj1ZuMVGU=" alt="Travel wish list from istockphoto.com" /></a>
+          <a href="/pages/wishlist">
+            <img
+              className="rounded-circle"
+              src="https://media.istockphoto.com/id/1058606496/photo/flat-lay-of-toy-airplane-with-pen-and-white-clean-paper-notepad-on-vivid-yellow-background.jpg?s=612x612&w=0&k=20&c=D2do0jwVaWGbr903qKeTegWr1Pvb7mCCE-Hj1ZuMVGU="
+              alt="Travel wish list from istockphoto.com"
+            />
+          </a>
           <div className="middle">
             {/* <Link to="/wishlist"><div className="text">Wishlist</div></Link> */}
           </div>
@@ -59,6 +87,6 @@ const Home = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Home;
