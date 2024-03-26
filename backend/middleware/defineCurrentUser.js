2dab06f5fd9e4a4b40d49d4a26b4fc53e45dd5a3
Tim <rtim10@ymail.com>
2024-03-25 20:44:33 -0400

    login

--- backend/middleware/defineCurrentUser.js
+++ backend/middleware/defineCurrentUser.js
@@ -1,26 +1,25 @@
-const db = require("../models")
-const jwt = require('json-web-token')
+const db = require("../models");
+const jwt = require('jsonwebtoken');
 
 const { User } = db;
 
-async function defineCurrentUser(req, res, next){
+async function defineCurrentUser(req, res, next) {
     try {
-        const [ method, token ] = req.headers.authorization.split(' ')
-        if(method == 'Bearer'){
-            const result = await jwt.decode(process.env.JWT_SECRET, token)
-            const { id } = result.value
-            let user = await User.findOne({ 
-                where: {
-                    userId: id
-                }
-            })
-            req.currentUser = user
+        const authHeader = req.headers.authorization;
+        if (authHeader && authHeader.startsWith('Bearer ')) {
+            const token = authHeader.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const { id } = decoded;
+            const user = await User.findById(id);
+            req.currentUser = user;
+        } else {
+            req.currentUser = null;
         }
-        next()
-    } catch(err){
-        req.currentUser = null
-        next() 
+        next();
+    } catch (err) {
+        req.currentUser = null;
+        next(err);
     }
 }
 
-module.exports = defineCurrentUser
+module.exports = defineCurrentUser;
--- frontend/src/forms/LoginForm.jsx
+++ frontend/src/forms/LoginForm.jsx
@@ -1,18 +1,55 @@
 // This form allows a registered user to sign in to their account
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
+import { useNavigate } from "react-router";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { CurrentUser } from "../contexts/CurrentUser";
 
 function LoginForm({show, handleClose}){
-    const [email, setEmail] = useState('');
+    
+    const navigate = useNavigate()
+
+    const { setCurrentUser } = useContext(CurrentUser)||{};
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [errorMessage, setErrorMessage] = useState(null)
 
     const clearForm = () => {
         setEmail('')
     }
 
-    const submitForm = () => {
+    async function submitForm(e)  {
+
+        e.preventDefault()
+
         const newUser = {
             email,
+            password
+        }
+
+        try {
+            const response = await fetch(`http://localhost:5050/authentication/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(email, password)
+            })
+
+            const data= await response.json()
+
+            if(response.ok) {
+                setCurrentUser(data.user)
+                localStorage.setItem('token', data.token);
+                navigate.push('/')
+            }else{
+                setErrorMessage(data.message)
+            }
+        } catch (error) {
+            console.error("An error occured:", error)
+            setErrorMessage("An error occured, please try again")
         }
 
 
@@ -53,12 +90,12 @@ function LoginForm({show, handleClose}){
 
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={email} onChange={e => setEmail({ email: e.target.value})}/>
                     <small id="emailHelp" class="form-text text-muted">* We'll never share your email with anyone else.</small>
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" value={password} onChange={e => setPassword({ password: e.target.value})}/>
                 </div>
                 
             </Modal.Body>
