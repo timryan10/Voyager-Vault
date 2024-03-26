5acc49def38ea40b9415c5c41b596f3879600ab1
Melanie Allmond <melanie.allmond@gmail.com>
2024-03-25 21:47:20 -0400

    hamburger box menu started

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,8 @@
 import {Link} from "react-router-dom";
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-// import Modal from 'react-bootstrap/Modal';
 import RegForm from '../forms/RegForm';
 import LoginForm from "../forms/LoginForm";
-// import SearchBar from "./SearchBar";
 
 
 function NavBar(){
@@ -18,17 +16,29 @@ function NavBar(){
     return (
         <div>
           <header>
-            <nav className="navigation">
-              <h1>Voyager Vault</h1>
-              <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/countries">Search Countries</Link></li>
-                <li><Link to="/wishlist">My Wishlist</Link></li>
-                <li><Link to="/destination">Destinations</Link></li>
-              </ul>
-              <div>
-                <Button className="formButton" variant="secondary" onClick={handleShow}>Register</Button>
-                <Button className="formButton" variant="success" onClick={handleShowLogin}>Log In</Button>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="/"><h1>Voyager Vault</h1></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-brand me-auto mb-2 mb-lg-1">
+                    {/* <li><Link to="/">Home</Link></li> */}
+                    <li><Link to="/countries">Search Countries</Link></li>
+                    <li><Link to="/wishlist">My Wishlist</Link></li>
+                    <li><Link to="/destination">Destinations</Link></li>
+                  </ul>
+                  <div>
+                    <Button className="formButton" variant="secondary" onClick={handleShow}>Register</Button>
+                    <Button className="formButton" variant="success" onClick={handleShowLogin}>Log In</Button>
+                  </div>
+                  {/* GENERAL SEARCH NAVBAR FIELD
+                    <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                  </form> */}
+                </div>
               </div>
             </nav>
           </header> 
