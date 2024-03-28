703650ec726df4278f0320a6086957c624b90eb7
Melanie Allmond <melanie.allmond@gmail.com>
2024-03-27 23:17:09 -0400

    Fixed functionality of menu toggler. Styling tomorrow.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,9 +7,6 @@ import Image from 'react-bootstrap/Image';
 import React from 'react';
 import logo from '../assets/Voyager-Vault-logo2.png';
 import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/js/dist/util';
-// import 'bootstrap/js/dist/dropdown';
 
 
 function NavBar(){
@@ -25,14 +22,13 @@ function NavBar(){
           <header>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
               <div className="container-fluid">
-                {/* <a className="navbar-brand" href="/"><h1>Voyager Vault</h1></a> */}
                 <a className="navbar-brand" href="/">
                   <img src={`${logo}`} height="90" alt="Voyager Vault company logo" />
                 </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01"> 
                   <ul className="navbar-brand me-auto mb-2 mb-lg-1">
                     <li className="nav-item active"><Link to="/countries">Search Countries</Link></li>
                     <li className="nav-item active"><Link to="/wishlist">My Wishlist</Link></li>
