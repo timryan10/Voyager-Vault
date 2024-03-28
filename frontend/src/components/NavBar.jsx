19be32bd7c153d4fa9c30e0825d804fbe12b9847
Melanie Allmond <melanie.allmond@gmail.com>
2024-03-27 20:51:25 -0400

    logo addition - notes and file only

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,7 +3,16 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import RegForm from '../forms/RegForm';
 import LoginForm from "../forms/LoginForm";
+import Image from 'react-bootstrap/Image';
+import React from 'react';
+import logo from '../assets/Voyager-Vault-logo.png';
 
+// console.log(logo); // /logo.84287d09.png
+
+// function Header() {
+//   // Import result is the URL of your image
+//   return <img src={logo} alt="Logo" />;
+// }
 
 function NavBar(){
   const [show, setShow] = useState(false);
@@ -19,12 +28,14 @@ function NavBar(){
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
               <div className="container-fluid">
                 <a className="navbar-brand" href="/"><h1>Voyager Vault</h1></a>
+                {/* <a className="navbar-brand" href="/">
+                  <Image src="../assets/Voyager-Vault-logo.png" height="80" alt="Voyager Vault company logo" />
+                </a> */}
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul className="navbar-brand me-auto mb-2 mb-lg-1">
-                    {/* <li><Link to="/">Home</Link></li> */}
                     <li className="nav-item active"><Link to="/countries">Search Countries</Link></li>
                     <li className="nav-item active"><Link to="/wishlist">My Wishlist</Link></li>
                     <li className="nav-item active"><Link to="/destination">Destinations</Link></li>
