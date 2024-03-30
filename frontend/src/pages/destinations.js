083a1926ed6b12df7fb4bce4cd9e49e3255ec9be
Melanie Allmond <melanie.allmond@gmail.com>
2024-03-30 03:27:58 -0400

    final modifications

--- frontend/src/pages/destinations.js
+++ frontend/src/pages/destinations.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { generalRequest } from '../httpService';
+import SearchBar from '../components/SearchBar';
 
 const Destination = () => {
   const [data, setData] = useState([])
@@ -25,10 +26,27 @@ const Destination = () => {
   }, [])
   return (
     <div>
-      <div className="homeBlock">
-        <a href="/pages/destinations"><img className="subpage-image" src="https://images.squarespace-cdn.com/content/v1/5f0780262fb7130a5ea990a7/1609721381739-1W27VHBAJHCN4W69TRGT/travel-list-banner.png?format=2500w" alt="Wishlist from postcardsfromaplanner.com" /></a>
+      <div className="homeBlock destinationDiv" style={{
+        backgroundImage: 'url("https://media.architecturaldigest.com/photos/6494807c22dde9e2fb285415/16:9/w_2560%2Cc_limit/GettyImages-136509187.jpg")',
+        bagkgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        objectFit: 'contain',
+        marginBottom: '20px',
+        marginTop: '0px'
+      }}>
+      <h2 className="search-h4">Memoirs of voyages past</h2>
       </div>
-      <h3>Cute tagline goes here!</h3>
+      <SearchBar />
+      <div style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          marginTop: "50px",
+          margin: "40px",
+          gap: "10px"
+        }}
+        >
       {data?.map((dest) => (
 
         <div className="card" style={{ width: '20rem' }}>
@@ -42,6 +60,7 @@ const Destination = () => {
         </div>
       ))
       }
+      </div>
     </div>
   );
 }
--- frontend/src/pages/wishlist.js
+++ frontend/src/pages/wishlist.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { generalRequest } from '../httpService';
+import SearchBar from '../components/SearchBar';
 
 const Wishlist = () => {
   const [data, setData] = useState([])
@@ -21,23 +22,27 @@ const Wishlist = () => {
 
   return (
     <div>
+      <h2>Embark on Dreams</h2>
       <div className="homeBlock">
         <a href="/pages/destinations"><img className="subpage-image" src="https://images.squarespace-cdn.com/content/v1/5f0780262fb7130a5ea990a7/1609721381739-1W27VHBAJHCN4W69TRGT/travel-list-banner.png?format=2500w" alt="Wishlist from postcardsfromaplanner.com" /></a>
       </div>
       <h3></h3>
+      <SearchBar />
       <div
         style={{
           display: "flex",
+          flexWrap: "wrap",
           justifyContent: "space-between",
           marginTop: "50px",
           margin: "40px",
+          gap: "10px"
         }}
       >
       {data?.map((wish) => (
         <div className="card wishlist-results" style={{ width: '20rem' }}>
           <div className="card-body">
             <h5 className="card-title">{wish.name}</h5>
-            <img src={wish.flag} alt={wish.name} style={{ width: '18rem' }} />
+            <img src={wish.flag} alt={wish.name} style={{ width: '16rem' }} />
             <p>Population: {wish.population}</p>
             <p>Capital: {wish.capital}</p>
 
